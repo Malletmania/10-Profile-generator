1a384cc354988793f3a28538d067d24e691c377c
Ben Camarillo <jtby97@gmail.com>
2021-10-16 22:20:29 -0500

    added what I had to document

---
+++ testfiles/engineer.test.js
@@ -0,0 +1,15 @@
+const Engineer = require('../jsfiles/engineer');
+
+test('creates an object', () => {
+    const Engineer = new Engineer('Benjamin', emp001, 'camarillolbenjamin@gmail.com', "Malletmania")
+
+    expect(engineer.GitHub).toEqual(expect.any(string));
+});
+
+test('askes for GibHub username', () => {
+    const Engineer = new Engineer('Benjamin', emp001, 'camarillolbenjamin@gmail.com', "Malletmania")
+
+    expect(engineer.getGithub()).toequal(expect.stringContaining(engineer.github.toString()));
+});
+
+// test('')
---
+++ testfiles/team.test.js
@@ -0,0 +1,6 @@
+const teamMember = require('../jsfiles/team');
+const teamMember = require('../jsfiles/team');
+
+TextDecoderStream('Should create employee objects to be passed', () => {
+    const teamMember = new teamMember('')
+})
